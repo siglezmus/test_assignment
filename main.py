f34b3afa22093f3b8226a3cf2d2409c08758f485
Erik Arutiunian <arutiunian.erik@seznam.cz>
2023-12-19 16:48:05 +0100

    Add argument parser

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 import logging
+import argparse
 from dirsync import sync
+from time import sleep
+
+parser = argparse.ArgumentParser()
+parser.add_argument("source_path")
+parser.add_argument("target_path")
+parser.add_argument("interval")
+parser.add_argument("log_file_path")
+args = parser.parse_args()
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-logger.addHandler(logging.FileHandler('log.file.log', mode='w'))
+log_file_path = args.log_file_path
 
-source_path = 'C:/Projects/base'
-target_path = 'C:/Projects/second'
+logger.addHandler(logging.FileHandler(log_file_path, mode='a'))
 
-sync(sourcedir=source_path, targetdir=target_path, action='sync', logger=logger) #for syncing one way
-# sync(sourcedir=target_path, targetdir=source_path, action='sync', logger=logger) #for syncing the opposite way
+source_path = args.source_path
+target_path = args.target_path
+
+interval = int(args.interval)
+
+logger.info(f"Args received: {args}")
+
+while True:
+    sync(sourcedir=source_path, targetdir=target_path, action='sync')
+    sleep(interval)
+
+def my_sync(sourcedir: str, targetdir: str, logger: logger):
+    pass
